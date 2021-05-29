e716dc7ffb987a3ddb18367e9cefe99bd270a44b
Jose Ortega <color.ui>
2021-05-29 12:10:18 -0400

    se crea poketscript.js

---
+++ pokescript.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    for(var a=1; a<=151; a++){
+        $('#pokemon').append('<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'+a+'.png">');
+    }
+})
